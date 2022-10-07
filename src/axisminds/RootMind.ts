ec3e3fd184b322d933d70e3b9790b7810bebd492
SwiftWombat <61484083+SwiftWombat@users.noreply.github.com>
2022-10-08 01:37:52 +1100

    feat: rootmind

--- src/axisminds/RootMind.ts
+++ src/axisminds/RootMind.ts
@@ -1,14 +1,30 @@
+import { Pyramidion } from "constructs/Pyramidion";
 import { AxisMind } from "./AxisMind";
 
 export class RootMind extends AxisMind
 {
+    subnets: { [name: string]: Pyramidion }; // hash of all payramidion subnets (key is the name of core pyramidion within the subnet)
+
+    constructor(host: {name: string, core: RoomObject})
+    {
+        super("Root", host);
+        this.subnets = {};
+        this.init();
+    }
+
     init(): void
     {
-
+        for (let name in this.subnets)
+        {
+            this.subnets[name].init()
+        }
     }
 
     run(): void
     {
-
+        for (let name in this.subnets)
+        {
+            this.subnets[name].run()
+        }
     }
 }
